b381fe95f8746017547737ba4ec81facb249bf68
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2026-03-30 13:51:28 -0300

    first commit

---
+++ src/main/java/br/com/home/api_flowable_labs/ApiFlowableLabsApplication.java
@@ -0,0 +1,13 @@
+package br.com.home.api_flowable_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFlowableLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFlowableLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/home/api_flowable_labs/ApiFlowableLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.home.api_flowable_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFlowableLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
